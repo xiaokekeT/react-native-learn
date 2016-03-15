b56fb902cb8f9c738e23f2bbbddfcdba3f10098e
Ryan <coolicer@foxmail.com>
2016-03-15 17:14:04 +0800

    完成

--- learn02/Components/NoteScreen.js
+++ learn02/Components/NoteScreen.js
@@ -1,6 +1,6 @@
 import React, {
      StyleSheet,
-     Text,
+     TextInput,
      View
    } from 'react-native';
 
@@ -8,7 +8,12 @@ export default class NoteScreen extends React.Component {
      render () {
        return (
          <View style={styles.container}>
-           <Text>Create Note Screen!</Text>
+            <TextInput ref="title"
+                autoFocus={true}
+                placeholder="Untitled"
+                onEndEditing={ (text)=>{ this.refs.body.focus() } }
+                style={ styles.title} />
+            <TextInput ref="body" placeholder="Start typing" multiline={true} style={ styles.body} />
          </View>
        );
     }
@@ -18,5 +23,12 @@ var styles = StyleSheet.create({
        flex: 1,
        justifyContent: 'center',
        alignItems: 'center',
+       marginTop: 64
+     },
+     title: {
+       height: 40
+     },
+     body: {
+       flex:1
      }
  });
--- learn02/index.ios.js
+++ learn02/index.ios.js
@@ -97,6 +97,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    
   },
 
 });
